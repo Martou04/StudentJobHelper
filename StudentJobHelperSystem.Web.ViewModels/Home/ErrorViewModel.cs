b7a49878b6ae28c2c4ac135e8e0cdde08deb4d6e
Martou04 <67951334+Martou04@users.noreply.github.com>
2023-07-31 20:29:19 +0300

    Add files via upload

---
+++ StudentJobHelperSystem.Web.ViewModels/Home/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentJobHelperSystem.Web.ViewModels.Home
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
